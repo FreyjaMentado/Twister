b03bed29d03fd575cae10034fe219e38289b6cd0
Freyja Shelton <freyjamentado@gmail.com>
2024-06-11 08:57:55 -0400

    terrain accelerates over time

--- terrain_controller.gd
+++ terrain_controller.gd
@@ -3,10 +3,11 @@ class_name TerrainController
 
 var TerrainBlocks: Array = []
 var terrain_belt: Array[MeshInstance3D] = []
-@export var terrain_velocity: float = 10.0
+@export var current_terrain_velocity: float = 10.0
 @export var max_terrain_velocity: float = 20.0
 @export var max_boost_velocity: float = 25.0
 @export var min_terrain_velocity: float = 10.0
+@export var terrain_acceleration: float = 1.0
 @export var num_terrain_blocks = 5
 @export_dir var terrian_blocks_path = "res://terrain_blocks"
 @export var player: CharacterBody3D
@@ -14,8 +15,8 @@ var terrain_belt: Array[MeshInstance3D] = []
 func _ready() -> void:
 	_load_terrain_scenes(terrian_blocks_path)
 	_init_blocks(num_terrain_blocks)
-	player.startBoost.connect(boost)
-	player.stopBoost.connect(reset_speed)
+	player.startBoost.connect(start_boost)
+	player.stopBoost.connect(stop_boost)
 	player.healthChanged.connect(reset_speed)
 
 func _physics_process(delta: float) -> void:
@@ -32,8 +33,13 @@ func _init_blocks(number_of_blocks: int) -> void:
 		terrain_belt.append(block)
 
 func _progress_terrain(delta: float) -> void:
+	if player.isBoosting:
+		current_terrain_velocity = move_toward(current_terrain_velocity, max_boost_velocity, terrain_acceleration * delta)
+	elif !player.isBoosting:
+		current_terrain_velocity = move_toward(current_terrain_velocity, max_terrain_velocity, terrain_acceleration * delta)
+	print("TV: ", current_terrain_velocity)
 	for block in terrain_belt:
-		block.position.z += terrain_velocity * delta
+		block.position.z += current_terrain_velocity * delta
 
 	if terrain_belt[0].position.z >= terrain_belt[0].mesh.size.y:
 		var last_terrain = terrain_belt[-1]
@@ -54,10 +60,13 @@ func _load_terrain_scenes(target_path: String) -> void:
 		print("Loading terrian block scene: " + target_path + "/" + scene_path)
 		TerrainBlocks.append(load(target_path + "/" + scene_path))
 
-func boost():
-	if terrain_velocity < max_boost_velocity:
-		terrain_velocity += 5
+func start_boost():
+	if current_terrain_velocity < max_boost_velocity:
+		current_terrain_velocity = max(current_terrain_velocity + 5, max_boost_velocity)
+
+func stop_boost():
+	current_terrain_velocity = max(current_terrain_velocity - 5, min_terrain_velocity)
 
 func reset_speed():
-	if terrain_velocity > min_terrain_velocity:
-		terrain_velocity = min_terrain_velocity
+	if current_terrain_velocity > min_terrain_velocity:
+		current_terrain_velocity = min_terrain_velocity
