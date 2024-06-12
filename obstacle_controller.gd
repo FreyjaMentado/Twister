66f5cd0a61410ed93befa331c7043bfc93e668b1
Freyja Shelton <freyjamentado@gmail.com>
2024-06-12 11:29:21 -0400

    now launches obstacles at the player

---
+++ obstacle_controller.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+var projectile = preload("res://sphere.tscn")
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	pass
+
+
+func _on_obstacle_timer_timeout():
+	var projCopy = projectile.instantiate()
+	add_child(projCopy)
+	var z = randi_range(55,65)
+	var x = randi_range(-10,10)
+	projCopy.apply_central_impulse(Vector3(x, 0, z))
+	var time = randf_range(.2,1.5)
+	print(time)
+	$ObstacleTimer.start(time)
